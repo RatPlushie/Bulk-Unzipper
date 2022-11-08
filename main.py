825a90f98571cb51942ea8947593603905cb2506
Aki <kieran.mortimer@gmail.com>
2022-11-08 15:44:08 +0100

    added error catching
    needed something to catch non-zip files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from zipfile import ZipFile
+from zipfile import ZipFile, BadZipFile
 
 # Getting working directory paths
 dir_path = os.getcwd()
@@ -9,16 +9,17 @@ output_folder_path = "{}/Output".format(dir_path)
 # Getting list of all files in the Input folder
 input_folder_list = os.listdir(input_folder_path)
 
-print("Bulk extraction beginning...")
-print()
+print("Bulk extraction beginning...\n")
 
 # Opening each zip file in the Input folder and extracting it to the output folder
 for file in input_folder_list:
-	print("Extracting {}".format(file))
+	try:
+		file_path = input_folder_path + "/" + file
+		with ZipFile(file_path, 'r') as zip:
+			print("Extracting {}".format(file))
+			zip.extractall(output_folder_path)
 
-	file_path = input_folder_path + "/" + file
-	with ZipFile(file_path, 'r') as zip:
-		zip.extractall(output_folder_path)
+	except BadZipFile:
+		print ("{} is not in .zip format".format(file))
 
-print()
-print("Bulk extraction complete")
+print("\nBulk extraction complete. See Output folder for results")
