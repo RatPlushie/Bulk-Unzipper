d40bbdcb811b4a5a1b131a136dd3cad94245aecd
Aki <kieran.mortimer@gmail.com>
2022-11-17 21:51:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import os
 from zipfile import ZipFile, BadZipFile
 
-# Getting working directory paths
-dir_path = os.getcwd()
-input_folder_path = "{}/Input".format(dir_path)
-output_folder_path = "{}/Output".format(dir_path)
+if __name__ == '__main__':
+	# Getting working directory paths
+	dir_path = os.getcwd()
+	input_folder_path = "{}/Input".format(dir_path)
+	output_folder_path = "{}/Output".format(dir_path)
 
-# Getting list of all files in the Input folder
-input_folder_list = os.listdir(input_folder_path)
+	# Getting list of all files in the Input folder
+	input_folder_list = os.listdir(input_folder_path)
 
-print("Bulk extraction beginning...\n")
+	print("Bulk extraction beginning...\n")
 
-# Opening each zip file in the Input folder and extracting it to the output folder
-for file in input_folder_list:
-	try:
-		file_path = input_folder_path + "/" + file
-		with ZipFile(file_path, 'r') as zip:
-			print("Extracting {}".format(file))
-			zip.extractall(output_folder_path)
+	# Opening each zip file in the Input folder and extracting it to the output folder
+	for file in input_folder_list:
+		try:
+			file_path = input_folder_path + "/" + file
+			with ZipFile(file_path, 'r') as zip:
+				print("Extracting {}".format(file))
+				zip.extractall(output_folder_path)
 
-	except BadZipFile:
-		print ("{} is not in .zip format".format(file))
+		except BadZipFile:
+			print ("{} is not in .zip format".format(file))
 
-print("\nBulk extraction complete. See Output folder for results")
+	print("\nBulk extraction complete. See Output folder for results")
